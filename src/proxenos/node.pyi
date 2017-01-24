8fde5dbc3710cb46268397a116d23514c15ca668
David Gidwani <david.gidwani@gmail.com>
2017-01-24 07:48:24 -0500

    fixed typing stubs for node

--- src/proxenos/node.pyi
+++ src/proxenos/node.pyi
@@ -14,3 +14,13 @@ class SocketAddress(object):
     def pack(self) -> typing.ByteString: ...
     def __bytes__(self) -> typing.ByteString: ...
     def __str__(self) -> str: ...
+
+
+class Service(object):
+    name = ...            # type: str
+    socket_address = ...  # type: SocketAddress
+    tags = ...            # type: Set[str]
+
+    def __init__(self, name: str,
+                 socket_address: SocketAddress,
+                 tags: Set[str]) -> None: ...
